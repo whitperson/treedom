478e5516af93c992aab68721840c3a53318e6af5
whitperson <whitperson@gmail.com>
2012-12-05 11:19:49 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treedom::Application.config.secret_token = '3e417132535084027c47f8dcce5f66a41a5e6a4adbbf70c61625e86b17d93db506069d180e67e119b2887cf7e4b1ce8905ef812310c97cc0f7b21d7b35ca8365'
